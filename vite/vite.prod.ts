cfc3e23b0dcdac99d3968dbab91a13b9a0aa4770
Громов Никита Андреевич <nagromov@dfo.ru>
2026-07-24 21:50:45 +0300

    fix: правка деплоя

--- vite/vite.prod.ts
+++ vite/vite.prod.ts
@@ -11,8 +11,10 @@ export default defineConfig(
       cssMinify: true,
       rollupOptions: {
         output: {
-          manualChunks: {
-            vendor: ['react', 'react-dom'],
+          manualChunks(id: string) {
+            if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+              return 'vendor';
+            }
           },
         },
       },
